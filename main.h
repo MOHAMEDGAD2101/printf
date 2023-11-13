1fd6845b6733001d014aca973aab7d434890fcb4
MOHAMEDGAD2101 <mohammedgad2101@gmail.com>
2023-11-13 12:49:50 -0800

    print f project

--- main.h
+++ main.h
@@ -1,22 +1,47 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <limits.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
 
-int _putchar(char character);
-int printf_char(va_list args, int printed);
-int printf_string(va_list args, int printed);
-int printf_integer(va_list args, int printed);
-int selector(const char *format, va_list args, int printed);
-int printf_binary(unsigned int num, int printed);
+
+
+/**
+ * struct format - converter for printf
+ * @ph: type char pointer of the specifier
+ * @function: function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *ph;
+	int (*function)();
+} convert;
+
+int *_strcpy(char *dest, char *src);
+int print_pointer(va_list val);
+int print_unsigned(va_list args);
+int print_hex_extra(unsigned long int num);
+int _strlenc(const char *s);
+int print_HEX_extra(unsigned int num);
+int print_exc_string(va_list val);
+int print_HEX(va_list val);
+int print_hex(va_list val);
+int print_oct(va_list val);
+int print_bin(va_list val);
+int print_revs(va_list args);
+int print_rot13(va_list args);
+int print_i(va_list args);
+int print_d(va_list args);
+int _strlen(char *s);
+int rev_string(char *s);
+int print_37(void);
+int print_c(va_list val);
+int print_s(va_list val);
+int _putchar(char c);
 int _printf(const char *format, ...);
-int _x(unsigned int num, int printed, int uppercase);
-int printf_octal(unsigned int num, int printed);
-int printf_unsigned(unsigned int num, int printed);
-int printf_reverse(va_list args, int printed);
-int printf_pointer(va_list args, int printed);
 
 #endif
